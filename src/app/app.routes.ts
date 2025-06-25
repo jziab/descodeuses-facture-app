9d543ed32c39fc3b43c58da7a2b58136af78e984
jziab <ziabjega98@gmail.com>
2025-06-25 17:23:52 +0200

    projet angular facture des codeuses app version 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { FactureListeComponent } from './facture-liste/facture-liste.component';
 
 export const routes: Routes = [
-    {path: 'facture', component: FactureListeComponent, title: 'facture'}
+    {path: '', component: FactureListeComponent, title: 'facture'}
 ];
