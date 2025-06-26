dd345512fe75db7eef15dd154ab02227cdcd016b
jziab <ziabjega98@gmail.com>
2025-06-26 11:17:14 +0200

    facture app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { provideHttpClient } from '@angular/common/http';
+import { ApiFactureService } from './service/api.facture.service';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      HttpClientInMemoryWebApiModule.forRoot(ApiFactureService,{delay:200})
+    ),
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
--- src/app/facture-liste/facture-liste.component.ts
+++ src/app/facture-liste/facture-liste.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatTabsModule} from '@angular/material/tabs';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 import { FactureService } from '../service/facture.service';
 
 @Component({
@@ -14,47 +19,47 @@ import { FactureService } from '../service/facture.service';
     MatTabsModule,
   ],
   templateUrl: './facture-liste.component.html',
-  styleUrl: './facture-liste.component.css'
+  styleUrl: './facture-liste.component.css',
 })
 export class FactureListeComponent implements OnInit {
-  
-signup!: FormGroup ;
+  signup!: FormGroup;
 
-constructor(private formbuilder: FormBuilder, private factureService : FactureService ){}
+  constructor(
+    private formbuilder: FormBuilder,
+    private factureService: FactureService,
+  ) {}
 
- tableau = [
- {designation:'Alfreds Futterkiste', prix: 1500, quantity: 1},
- {designation:'Centro comercial Moctezuma', prix:2000, quantity: 1 },
- {designation:'Ernst Handel', prix:3000, quantity: 1},
- {designation:'Island Trading', prix:1500, quantity: 2},
- {designation:'Laughing Bacchus Winecellars', prix:1500, quantity: 2}
- ]
- somme = 0; 
+  tableau = [
+    { designation: 'Alfreds Futterkiste', prix: 1500, quantity: 1 },
+    { designation: 'Centro comercial Moctezuma', prix: 2000, quantity: 1 },
+    { designation: 'Ernst Handel', prix: 3000, quantity: 1 },
+    { designation: 'Island Trading', prix: 1500, quantity: 2 },
+    { designation: 'Laughing Bacchus Winecellars', prix: 1500, quantity: 2 },
+  ];
+  somme = 0;
 
- ngOnInit(): void {
-  this.signup = this.formbuilder.group({
-      designation:['',[Validators.required]],
-      prix:['',[Validators.required]],
-      quantity:['',[Validators.required]]
-  })
- this.calcul();
- }
- onsubmit(){
-  if (this.signup.valid) {
-    this.tableau.push(this.signup.value)
-    this.somme = 0
-      this.calcul();
-    console.log(this.signup.value);
+  ngOnInit(): void {
+    this.signup = this.formbuilder.group({
+      designation: ['', [Validators.required]],
+      prix: ['', [Validators.required]],
+      quantity: ['', [Validators.required]],
+    });
+    this.calcul();
   }
-
- }
- calcul(){
- let prixFinal = 0 ; 
-    for (let i = 0; i < this.tableau.length; i++) {
-     prixFinal = this.tableau[i].quantity * this.tableau[i].prix 
-   this.somme +=  prixFinal;
+  onsubmit() {
+    if (this.signup.valid) {
+      this.tableau.push(this.signup.value);
+      this.somme = 0;
+      this.calcul();
+      console.log(this.signup.value);
     }
-   console.log("la somme est : "+ this.somme);
- }
- 
+  }
+  calcul() {
+    let prixFinal = 0;
+    for (let i = 0; i < this.tableau.length; i++) {
+      prixFinal = this.tableau[i].quantity * this.tableau[i].prix;
+      this.somme += prixFinal;
+    }
+    console.log('la somme est : ' + this.somme);
+  }
 }
